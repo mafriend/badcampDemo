6c8b0337911e9c61ac7bd3d7dc998196fb73fa75
Monica Friend <monicaafriend@gmail.com>
2018-10-20 11:49:29 -0500

    broke out methods

--- src/main/java/classes/LoginPage.java
+++ src/main/java/classes/LoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
 
-    private WebDriver driver;
+    private static WebDriver driver;
     private Object currentPage;
 
     public void startDriver() {
@@ -15,27 +15,20 @@ public class LoginPage {
     }
 
     public static void visitUmamiSite() {
-        WebDriver driver = new ChromeDriver();
         driver.get("http://127.0.0.1:8888/");
-        //locateUmamiLogo
-        WebElement logo = driver.findElement(By.xpath("//img[contains(@src,'/core/profiles/demo_umami/themes/umami/logo.svg')]"));
-        //clickLoginButton
-        WebElement loginButton = driver.findElement(By.cssSelector("a[href='/user/login']"));
-        loginButton.click();
-        //enterUsername
-        WebElement usernameField = ((ChromeDriver) driver).findElementById("id='edit-name'");
-        usernameField.click();
-
     }
 
     public static void locateUmamiLogo() {
-//        WebElement logo = driver.findElement(By.xpath("//img[contains(@src,'/core/profiles/demo_umami/themes/umami/logo.svg')]"));
+        WebElement logo = driver.findElement(By.xpath("//img[contains(@src,'/core/profiles/demo_umami/themes/umami/logo.svg')]"));
     }
 
     public static void clickLoginButton() {
-
+        WebElement loginButton = driver.findElement(By.cssSelector("a[href='/user/login']"));
+        loginButton.click();
     }
 
     public static void enterUsername() {
+        WebElement usernameField = ((ChromeDriver) driver).findElementById("id='edit-name'");
+        usernameField.click();
     }
 }
